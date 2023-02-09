16d8f0d9b8b26c3631e9bbe8131b33c2c693eefb
wambexpeter <wambexpeter@gmail.com>
2023-02-09 16:39:38 +0300

    Encaps

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("peter");
+        //create methode "RWOnly" as an error appears.
+        System.out.println(obj.getName());
+    }
+
 }
